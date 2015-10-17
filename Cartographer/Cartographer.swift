774ce6c484c7fa3811b23858bec43bf74caddae5
cscalcucci <cscal@aphelion-enterprises.com>
2015-10-17 18:32:38 -0400

    Initial Commit

---
+++ Cartographer/Cartographer.swift
@@ -0,0 +1,9 @@
+//
+//  Cartographer.swift
+//  Cartographer
+//
+//  Created by Christopher Scalcucci on 10/17/15.
+//  Copyright © 2015 Christopher Scalcucci. All rights reserved.
+//
+
+import Foundation
